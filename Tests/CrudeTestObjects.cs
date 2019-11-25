4f5f4ba7d149ebaa56285dceb1634681d69b94cf
Mike.McLean <Mike.McLean@softwire.com>
2019-11-25 09:34:54 +0000

    (I) Initial high-level tests.

---
+++ Tests/CrudeTestObjects.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using LochNessBuilder;
+// ReSharper disable InconsistentNaming
+
+namespace Tests
+{
+    public class TestObject
+    {
+        public int Id { get; set; }
+        public int IntProp { get; set; }
+        public string StringProp { get; set; }
+        public object ObjectProp { get; set; }
+
+        public int[] ArrayProp { get; set; }
+        public List<int> ListProp { get; set; }
+        public IList<int> IListProp { get; set; }
+        public Collection<int> CollectionProp { get; set; }
+        public ICollection<int> ICollectionProp { get; set; }
+        public Queue<int> QueueProp { get; set;}
+        public Stack<int> StackProp { get; set; }
+        public IEnumerable<int> IEnumerableProp { get; set; }
+        public IQueryable<int> IQueryableProp { get; set; }
+        public HashSet<int> HashSetProp { get; set; }
+        public ISet<int> ISetProp { get; set; }
+        public ReadOnlyCollection<int> ReadOnlyCollectionProp { get; set; }
+        public IReadOnlyCollection<int> IReadOnlyCollectionProp { get; set; }
+
+        public TestSubObject SubObjectProp { get; set; }
+        public int SubObjectRef { get; set; }
+    }
+
+    public class TestSubObject
+    {
+        public int Id { get; set; }
+        public string StringProp { get; set; }
+
+        public TestObject AssociatedTestObject { get; set; }
+    }
+
+    [BuilderFactory]
+    public static class RegisteredSubObjectBuilder
+    {
+        public static Builder<TestSubObject> Other => Builder<TestSubObject>.New.With(subObj => subObj.StringProp, "OtherBuilder");
+        public static Builder<TestSubObject> New => Builder<TestSubObject>.New.With(subObj => subObj.StringProp, "NewBuilder");
+    }
+}
