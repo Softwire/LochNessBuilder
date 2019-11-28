aee3ed77a282d23f7543e75b7afa0e6503b29131
Mike.McLean <Mike.McLean@softwire.com>
2019-11-28 11:23:15 +0000

    Clearer Error message

--- Tests/WithFactory_Enumerable_Tests.cs
+++ Tests/WithFactory_Enumerable_Tests.cs
@@ -141,7 +141,11 @@ namespace Tests
         {
             Action builderSetupAction = () => Builder<TestObject>.New.WithFactory(o => o.SubObjectListProp, () => new object());
             builderSetupAction.Should().Throw<NotSupportedException>()
-                .WithMessage("*From the object values provided, the IEnumerable handler knows how to create object[], List<object>, HashSet<object>, Queue<object>, Collection<object>, ReadOnlyCollection<object>, or IQueryable<object>. Your property type can't be populated by any of those types, and is thus unsupported by this method. Please use a standard .With() call.*")
+                // Message explains general concept.
+                .WithMessage("*From the object values provided, the IEnumerable handler knows how to create*")
+                .WithMessage("*Your property type can't be populated by any of those types, and is thus unsupported by this method.*")
+                // Message clarifies specific details.
+                .WithMessage("*object[], List<object>, HashSet<object>, Queue<object>*")
                 .WithMessage("*List`1[Tests.TestSubObject]*");
         }
     }
